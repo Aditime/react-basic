8f2936a9d7d74d0ca94ec6a0290517f6a7502d60
Aditi <barua.aditi.cs@gmail.com>
2021-02-27 11:48:03 -0700

    changes from section 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Person from "./Person/Person";
 
 /*
+
+App.js is a comtainer which extends component and it has a state property, 
 JSX restriction - 
 you need to change certain keywords, for example, class to className in the html like tag
 You can only have one root element.
@@ -21,82 +23,102 @@ Passing parameter to onClick - there are two ways
 
 */
 
-
 class App extends Component {
-state = {
-  persons: [
-    {name : 'Max', age : 28},
-    {name : 'Max', age : 29},
-    {name : 'Max', age : 24}
-  ]
-}
-
-switchNameHandler = (newname) =>{
- // console.log("Was clicked");
- // Dont do this this.state.persons[0].name = 'Aditi Barua';
- this.setState(
-  {
+  state = {
     persons: [
-      {name : newname, age : 28},
-      {name : 'menu', age : 29},
-      {name : 'steve', age : 23}
-    ]
-  }
- )
-}
+      { id: "1", name: "Aditi", age: 28 },
+      { id: "2", name: "Max", age: 29 },
+      { id: "3", name: "Steve", age: 24 },
+    ],
+    otherState: "Some other value",
+    showPersons: false,
+  };
 
-nameChangedHandler = (event) =>{
-  this.setState(
-    {
+  /*switchNameHandler = (newname) => {
+    // console.log("Was clicked");
+    // Dont do this this.state.persons[0].name = 'Aditi Barua';
+    this.setState({
       persons: [
-        {name : 'Max', age : 28},
-        {name : event.target.value, age : 29},
-        {name : 'steve', age : 23}
-      ]
-    }
-   )
-}
+        { name: newname, age: 28 },
+        { name: "menu", age: 29 },
+        { name: "steve", age: 23 },
+      ],
+    });
+  };*/
 
-   render() {
-     const style = {
-      backgroundColor : 'white',
-      font : 'inherit',
-      border: '1px solid blue',
-      padding : '8px'
-     };
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      // p is every person in the person state
+      return p.id === id;
+    });
+
+    const person = {
+      // the spread operator is copying the stete to prevent mutation.
+      ...this.state.persons[personIndex],
+    };
+
+    // const person = Object.assign({}, this.state.persons[personIndex]); this is for copying the state.
+
+    person.name = event.target.value; // we are assigning the name of the person to what we got from input
+    const persons = [...this.state.persons]; // copy the state into persons.
+    persons[personIndex] = person; // assign the person in the persons in the perticular index
+    this.setState({ persons: persons }); // the persons back to the state.
+  };
+
+  //update states in not changed approach
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons;  // this mutates the original state which is not a good practice. Below are the two solution.
+    // const persons = this.state.persons.slice(); // this creates a copy of the original states.
+    const persons = [...this.state.persons]; //this is the spread operator, this creates a copy of the original state
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
+  render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+    };
+
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id} //unique identifier so that React can identify all the components.
+                Changed={(event) => this.nameChangedHandler(event, person.id)}
+              />
+            );
+          })}
+        </div>
+      );
+    }
 
     return (
-    <div className='App'> 
-      <h1> This is my very first React App. It just covers basic React structure and features</h1>
-      <button 
-      style={style}
-      onClick={ () => this.switchNameHandler('aditi barua') }>Switch here </button>
-      <Person 
-      name ={this.state.persons[0].name} 
-      age = {this.state.persons[0].age}  />
-      <Person 
-      name ={this.state.persons[1].name}
-      age = {this.state.persons[1].age} 
-      click = {this.switchNameHandler.bind(this, 'abarua')}
-      Changed = {this.nameChangedHandler}>
-        Hobby : Dancing</Person>
-      <Person 
-      name ={this.state.persons[2].name} 
-      age = {this.state.persons[2].age}  />
-      </div> 
+      <div className="App">
+        <h4>Click each component to make the disappear</h4>
+        <button style={style} onClick={this.togglePersonsHandler}>
+          Toggle Components
+        </button>
+        {persons}
+      </div>
     );
-//return React.createElement('div', {className: 'App'}, React.createElement('h1',null, 'This is really working!!!'));
-
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1',null, 'This is really working!!!'));
   }
 }
 export default App;
-
-
-
-
-
-
-
 
 /*
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import './Person.css';
-
+import React from "react";
+import "./Person.css";
 
 /*
 CSS styling : how to style css. 
@@ -9,13 +8,15 @@ But if you inspect the code behind in the browser, you will see the CSS, that
 is because webpack bundles the css behind the scene. 
 * */
 const person = (props) => {
-    return (
-    <div className ="Person"> 
-    <p onClick ={props.click}>I am {props.name} and I am {props.age} years old</p>
-    <p>{props.children}</p>
-    <input type = "text" onChange={props.Changed} value = {props.name}></input> 
+  return (
+    <div className="Person">
+      <p onClick={props.click}>
+        I am {props.name} and I am {props.age} years old
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.Changed} value={props.name}></input>
     </div>
-    )
+  );
 };
 
 export default person;
